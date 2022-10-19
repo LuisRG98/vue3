2166573fa60c174fde5dc72ba1f4045dd095966e
Luis Rivas <70450227+LuisRG98@users.noreply.github.com>
2022-10-19 13:41:51 -0400

    practica2

---
+++ tarea2/src/components/AppPiePag.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer v-bind:style="{ backgroundColor: footerColor}">
+        asdsadsa
+    </footer>
+</template>
+  
+
+<script>
+export default {
+    name: 'appPiePag',
+    data() {
+        return{
+            footerColor: "slategrey",
+        }
+
+    },
+    components: {
+
+    }
+}
+</script>
+  
+
+<style>
+
+</style>
